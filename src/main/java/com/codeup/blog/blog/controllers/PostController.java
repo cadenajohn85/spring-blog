5efe348651dd1a6aa8ab01eb940b402cb8712bf1
John Cadena <cadena.john.85@gmail.com>
2019-11-05 14:16:58 -0600

    create Post entity; update PostController logic to use Post objects (Views Exercise part 3)

--- src/main/java/com/codeup/blog/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/blog/controllers/PostController.java
@@ -1,31 +1,32 @@
 package com.codeup.blog.blog.controllers;
 
+import com.codeup.blog.blog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
-//    Create a PostController class. This controller should return strings for the following routes that describe what will ultimately be there.
-//    method	url	description
-//    GET	/posts	posts index page
-//    GET	/posts/{id}	view an individual post
-//    GET	/posts/create	view the form for creating a post
-//    POST	/posts/create	create a new post
-
     @GetMapping("/posts")
-    @ResponseBody
-    public String getPosts() {
-        return "posts index page";
+    public String getAllPosts(Model vModel) {
+        ArrayList<Post> postsList = new ArrayList<>();
+        postsList.add(new Post("My first post", "Very cool content."));
+        postsList.add(new Post("A more recent post", "Even cooler content."));
+        vModel.addAttribute("posts", postsList);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String getPostsById(@PathVariable long id) {
-        return "view an individual post";
+    public String getPostById(@PathVariable long id, Model vModel) {
+        Post postToView = new Post("Title of my post", "Super-engaging content");
+        vModel.addAttribute("post", postToView);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
