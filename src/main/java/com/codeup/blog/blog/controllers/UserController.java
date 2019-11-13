a0ae8725e4db4f4168d16da1ab83c31aa9638dbf
John Cadena <cadena.john.85@gmail.com>
2019-11-13 11:32:08 -0600

    add log-in and create user functionality via Spring Security

---
+++ src/main/java/com/codeup/blog/blog/controllers/UserController.java
@@ -0,0 +1,38 @@
+package com.codeup.blog.blog.controllers;
+
+import com.codeup.blog.blog.models.User;
+import com.codeup.blog.blog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    private UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model vModel) {
+        vModel.addAttribute("user", new User());
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
+
+
+
+}
--- src/main/java/com/codeup/blog/blog/models/User.java
+++ src/main/java/com/codeup/blog/blog/models/User.java
@@ -32,6 +32,13 @@ public class User {
         this.password = password;
     }
 
+    public User(User copy) {
+        id = copy.id;
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
     public long getId() {
         return id;
     }
